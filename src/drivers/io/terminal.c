2122d981586927a653f55fdb4d493d4053b3fe6f
Michael Petch <mpetch@capp-sysware.com>
2017-04-03 12:49:34 -0600

    Fix formatting slightly

--- src/drivers/io/terminal.c
+++ src/drivers/io/terminal.c
@@ -17,8 +17,8 @@ void term_enable_cursor(void)
 
 	outb(0x3D4, 0x0B);
 	char prevval = inb(0x3D5) & ~(0x1F);
-							// get byte that has the current scanline end (lower 5 bits) and set them to 0
-							// we want to keep the upper 3 bits unchanged
+					// get byte that has the current scanline end (lower 5 bits) and set them to 0
+					// we want to keep the upper 3 bits unchanged
 	prevval |= 0x0e;		// set the end scanline to 14(0x0e).
 	outb(0x3D4, 0x0B);
 	outb(0x3D5, prevval);
