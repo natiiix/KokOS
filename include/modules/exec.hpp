e77f0df9b3f55839bcd8a4da77fa4f8f2104f846
natiiix <natiwastaken@gmail.com>
2017-05-05 00:25:57 +0200

    Fixed comments

--- include/modules/exec.hpp
+++ include/modules/exec.hpp
@@ -39,29 +39,29 @@ public:
 
 private:
     vector<vector<string>> m_program; // lines of code
-    vector<Variable> m_variables;
+    vector<Variable> m_variables; // vector of all currently declared variables
 
     size_t m_counter; // program counter
     size_t m_scope; // current scope depth
-    vector<size_t> m_scopeStack; // contains indexes at which the program pushed the scope (to be used by loops)
+    vector<size_t> m_scopeStack; // contains indexes at which the program pushed the scope (used by while loop)
 
-    void scopePush(void);
-    void scopePop(void);
+    void scopePush(void); // pushes current program counter onto the scope stack and incremenets the scope level
+    void scopePop(void); // decrements the scope level and disposes all variables that were declared in that scope
 
     void executeCommand(void); // executes the command at the current line
 
-    void varDeclare(const string& name, const DataType type);
-    Variable* varFind(const string& name);
+    void varDeclare(const string& name, const DataType type); // declares a new variable with a specified name and data type
+    Variable* varFind(const string& name); // finds a variable by name and returns a pointer to it, returns nullptr on failure
     bool varNameIsKeyword(const string& name); // check variable name against a list of keywords
     bool varNameInvalidChar(const string& name); // check if variable name consists of valid characters only
 
     void exit(void); // stops the program execution
     void error(const char* const str); // prints an error message and exits the program
-    void error(const string& str);
+    void error(const string& str); // overload of error() to make it easier to use string objects
     void errorVarUndeclared(const string& name); // "variable hasn't been declared" error
     void errorSymbolUnresolved(const string& name); // "unable to resolve symbol" error
-    void errorTypesIncompatible(void);
-    void errorOperatorInvalid(const string& strOperator);
+    void errorTypesIncompatible(void); // "variables don't have matching data types" error
+    void errorOperatorInvalid(const string& strOperator); // "symbol is not a valid operator" error
 
     bool symbolToInteger(const string& strSymbol, INTEGER* const output, const bool throwError = true);
     bool symbolToLogical(const string& strSymbol, LOGICAL* const output, const bool throwError = true);
