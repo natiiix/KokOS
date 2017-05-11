2ab3f85af4b6cebd8ce0de5e22eeddf3a8fa11f2
natiiix <natiwastaken@gmail.com>
2017-05-11 02:15:17 +0200

    Memory: Decreased dynamic segment size to 16 Bytes

--- src/drivers/memory.c
+++ src/drivers/memory.c
@@ -28,7 +28,7 @@ const size_t DYNAMIC_SEGMENT_LIMIT = MEMORY_SIZE_BYTES / DYNAMIC_SEGMENT_SIZE;*/
 #define STATIC_SEGMENT_LIMIT 1024
 
 // Dynamic memory is being allocated / deallocated DYNAMIC_SEGMENT_SIZE bytes at a time
-#define DYNAMIC_SEGMENT_SIZE 256
+#define DYNAMIC_SEGMENT_SIZE 0x10
 #define DYNAMIC_SEGMENT_LIMIT (MEMORY_SIZE_BYTES / DYNAMIC_SEGMENT_SIZE)
 
 size_t mapPage = 0;
